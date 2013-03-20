13369c183c97ecc2aee2bae3aafe06278c311cb3
keyston <keyston@conceptual-ideas.com>
2013-03-19 21:31:30 -0500

    - Setup DataNum

--- src/main/scala/com/rethinkdb/Ast.scala
+++ src/main/scala/com/rethinkdb/Ast.scala
@@ -2,6 +2,8 @@ package com.rethinkdb
 
 import ql2.Ql2._
 import scala.concurrent.ExecutionContext.Implicits.global
+import ql2.Ql2.Term;
+import ql2.Ql2.Query.AssocPair
 
 /**
  * Created by IntelliJ IDEA.
@@ -9,6 +11,36 @@ import scala.concurrent.ExecutionContext.Implicits.global
  * Date: 3/19/13
  * Time: 12:06 PM 
  */
+
+
+
+trait TermBase{
+  private var args:Seq[TermBase] = Seq.empty[TermBase]
+  private var optargs:Seq[(String,DataNum)]=Seq.empty[(String,DataNum)]
+  val termType:Term.TermType
+  def run()={
+    None
+  }
+
+  implicit def termBaseToTerm(base:TermBase):Term = {
+
+    base.compile(Term.newBuilder().build())
+
+
+  }
+  implicit def optargToAssocPair(i:(String,DataNum)):AssocPair={
+
+    AssocPair.newBuilder().setKey(i._1).setVal(i._2).build()
+  }
+  def compile(term:Term):Term={
+    val builder = term.toBuilder.setType(termType)
+    for (a <- args)builder.addArgs(a)
+    //for (o <- optargs) builder.addOptargs(o)
+    builder.build()
+
+
+  }
+}
 trait BaseQuery {
 
 
@@ -32,7 +64,40 @@ class T(args: Seq[AnyRef], inspect: String = "") extends Iterator[AnyRef] {
 sealed trait ExprWrap;
 
 
-case class DataNum(data:AnyRef)
+abstract class DataNum(data:Any) extends TermBase{
+
+  implicit def boolToDatNum
+  override def compile(term:Term):Term={
+    val builder = term.toBuilder
+    val a:Boolean =true
+    val datumBuilder = builder.getDatumBuilder
+    data match{
+      case None=>
+      case b:Boolean=>datumBuilder.setType(Datum.DatumType.R_BOOL).setRBool(b)
+      case i @(Int | Float | Long) =>datumBuilder.setType(Datum.DatumType.R_NUM).setRNum(i match{
+        case a:Int =>a.toDouble
+        case b:Float => b.toDouble
+
+        case c:Long => c.toDouble
+      })
+      case s:String => datumBuilder.setType(Datum.DatumType.R_STR).setRStr(s)
+      case _=> throw new RuntimeException("Cannot build a query")
+
+    }
+    builder.build()
+  }
+}
+
+trait DatumApplier{
+
+  def apply(d:Datum)
+}
+
+
+object DataNum{
+
+
+}
 
 case class Var(name: String) {
   val term = Term.TermType.VAR
--- src/main/scala/com/rethinkdb/Net.scala
+++ src/main/scala/com/rethinkdb/Net.scala
@@ -1,6 +1,6 @@
 package com.rethinkdb
 
-import com.rethinkdb.{DataNum,DB}
+
 
 /**
  * Created by IntelliJ IDEA.
@@ -8,10 +8,7 @@ import com.rethinkdb.{DataNum,DB}
  * Date: 3/19/13
  * Time: 12:02 PM 
  */
-case class Connection(host: String, port: Int, db: String = "test")
-  private val _db =DB(db)
-
-object Connection {
+case class Connection(host: String, port: Int, db: String = "test") {
 
 
 }
