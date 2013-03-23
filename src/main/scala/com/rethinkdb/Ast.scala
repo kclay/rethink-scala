f2475d140567c4bd1e5e20502efd8193481dc62d
keyston <keyston@conceptual-ideas.com>
2013-03-22 20:59:26 -0500

    - Covered all AST token printing

--- src/main/scala/com/rethinkdb/Ast.scala
+++ src/main/scala/com/rethinkdb/Ast.scala
@@ -11,27 +11,80 @@ import ql2.{Ql2 => p}
  * Time: 12:06 PM 
  */
 
-
-object Ast {
-
+object Tokens{
 
   sealed trait Token
   sealed trait TermBlock extends Token
-  trait TokenType{
+  trait TokenType extends Token{
+    type T
     def name:String
-    def value:Either[p.Term.TermType,p.Datum.DatumType]
+    def value:T
   }
 
-  case class TermTokenType(termType:p.Term.TermType) extends TokenType with Token{
+  case class TermTokenType(termType:p.Term.TermType) extends TokenType {
+    type T =p.Term.TermType
     def name:String =termType.name
-    def value = Left(termType)
+    def value = termType
   }
 
-  case class DatumTokenType(datumType:p.Datum.DatumType) extends TokenType with Token{
+  case class DatumTokenType(datumType:p.Datum.DatumType) extends TokenType {
+    type T = p.Datum.DatumType
     def name:String = datumType.name
-    def value = Right(datumType)
+    def value = datumType
   }
- // implicit def tokenTypeToTermType(tokenType:Either[p.Term.TermType,p.Datum.DatumType]):p.Term.TermType =tokenType.left
+  trait AssocPairToken extends Token{
+    import Ast.Expr
+    val key:String
+    val value:Any
+    lazy val token =Expr(value)
+
+
+    import Ast.Expr
+
+    type T
+
+
+    trait Builder{
+
+      val get:Any
+    }
+
+
+    def pair:T
+
+
+
+  }
+  case class TermAssocPairToken(key:String,value:Any) extends AssocPairToken{
+
+    type T = p.Term.AssocPair
+
+
+    private lazy val builder=new Builder{
+
+      val get = p.Term.AssocPair.newBuilder.setKey(key).setVal(token.compile().asInstanceOf[p.Term]).build
+    }
+    def unwrap:p.Term.AssocPair = pair.asInstanceOf[p.Term.AssocPair]
+    def pair:T = builder.get.asInstanceOf[T]
+
+
+  }
+
+  case class DatumAssocPairToken(key:String,value:Any) extends AssocPairToken{
+    type T = p.Datum.AssocPair
+
+    private lazy val builder=new Builder{
+
+      val get = p.Datum.AssocPair.newBuilder.setKey(key).setVal(token.compile().asInstanceOf[p.Datum]).build
+    }
+    def unwrap:p.Datum.AssocPair = pair.asInstanceOf[p.Datum.AssocPair]
+    def pair:T = builder.get.asInstanceOf[T]
+
+  }
+
+  case class Args(args:Iterable[Token]) extends Token
+  case class OptArgs(optargs:Iterable[AssocPairToken]) extends Token
+  // implicit def tokenTypeToTermType(tokenType:Either[p.Term.TermType,p.Datum.DatumType]):p.Term.TermType =tokenType.left
 
 
   implicit def termType2TokenType(termType:p.Term.TermType):TermTokenType = TermTokenType(termType)
@@ -39,6 +92,16 @@ object Ast {
 
   implicit def datumType2TokenType(datumType:p.Datum.DatumType):DatumTokenType = DatumTokenType(datumType)
   implicit def datumTokenType2DatumType(token:DatumTokenType):p.Datum.DatumType = token.datumType
+
+  implicit def args2Token(args:Iterable[Token]):Args = Args(args)
+
+  implicit def optArgs2Token(optargs:Iterable[AssocPairToken]) = OptArgs(optargs)
+}
+
+object Ast {
+
+
+  import com.rethinkdb.Tokens._
 
   trait Composable {
     def compose(args: Seq[Term], optargs: Map[String, Term]) = {
@@ -61,30 +124,50 @@ object Ast {
   }*/
 
   object Expr {
+    /*import reflect._
+    class Def[C](implicit desired : Manifest[C]) {
+       def unapply[X](c : X)(implicit m : Manifest[X]) : Option[C] = {
+           def sameArgs = desired.typeArguments.zip(m.typeArguments).forall {case (desired,actual) => desired >:> actual}
+           if (desired >:> m && sameArgs) Some(c.asInstanceOf[C])
+           else None
+         }
+       }
 
-    def apply(term: Term) = term
+    val DefMap=new Def[Map[String,Any]]
+    val DefSeq=new Def[Seq[Any]]    */
+    def apply(term: Term):Term = term
 
-    def apply(a: Any): Term = a match {
-      case t: Term => t
-      //case s: Seq[Any] => MakeArray(s)
-      //case m: Map[String, Any] => MakeObj(m)
-      // case f: PartialFunction =>
-      //case a: Any => DatNum(a)
+    def apply(value:Seq[Any]):Term = MakeArray(value)
+    def apply(value:Map[String,Any]):Term = MakeObj(value)
+    def apply(a: Any): Term = {
+      val b = a
+      a match{
+        case t:Term => t
+        case s:Seq[_]=>MakeArray(s)
+        case m:Map[_,_]=>MakeObj(m.asInstanceOf[Map[String,Any]])
+        case a:Any => Datum(a)
+
+
+      }
     }
+
   }
 
   trait Term extends TermBlock{
     def ast:String = "Term"
     lazy val args = Seq.empty[Term]
 
+
     protected def buildArgs(args: Any*) = for (a <- args) yield Expr(a)
 
-    lazy val optargs = Map.empty[String, Term]
+    lazy val optargs = Iterable.empty[AssocPairToken]
 
 
-    protected def buildOptArgs(optargs: Map[String, Any]) = optargs.filter(_._2 != None) map {
-      case (key: String, value: Any) => (key, Expr(value))
+    protected def buildOptArgs(optargs: Map[String, Any]):Iterable[AssocPairToken] = optargs.filter(_._2 != None) collect {
+      case (key: String, value: Any) =>  optArgsBuilder (key,value)
     }
+    def optArgsBuilder(key:String,value:Any):AssocPairToken =  TermAssocPairToken(key,value)
+
 
     def termType: TokenType
 
@@ -95,7 +178,7 @@ object Ast {
 
     implicit def termBaseToTerm(base: Term): p.Term = {
 
-      base.compile(p.Term.newBuilder().build())
+      base.compile(Some(p.Term.newBuilder().build()))
 
 
     }
@@ -105,12 +188,14 @@ object Ast {
       p.Term.AssocPair.newBuilder().setKey(i._1).setVal(i._2).build()
     }
 
-    def compile(term: p.Term): p.Term = {
-     // val builder = term.toBuilder.setType(termType.value)
-      // for (a <- args) builder.addArgs(a)
-      //for (o <- optargs) builder.addOptargs(o)
-      //builder.build()
-      term
+    type TermType = p.Term.TermType
+    def compile(term: Option[p.Term]=None): p.Term = {
+      val builder = term.map(_.toBuilder).getOrElse(p.Term.newBuilder())
+        .setType(termType.value.asInstanceOf[TermType])
+     for (a <- args) builder.addArgs(a)
+    // for (o <- optargs) builder.addOptargs(o)
+      builder.build()
+
 
 
     }
@@ -176,23 +261,39 @@ object Ast {
 
   sealed trait Datum extends Term with ExprWrap with Composable {
 
-    override def compile(term: p.Term): p.Term = {
-      val builder = term.toBuilder
+    override def compile(term:Option[p.Term]=None ): p.Term = {
+      val builder = term.get.toBuilder
       build(builder.getDatumBuilder)
 
       builder.build()
     }
+    override def optArgsBuilder(key:String,value:Any):AssocPairToken =  DatumAssocPairToken(key,value)
 
-    def termType = p.Term.TermType.DATUM
+    def termType:TokenType = p.Term.TermType.DATUM
 
     def build(builder: p.Datum.Builder): p.Datum.Builder
   }
+  object Datum{
+
+    def apply(a:Any):Datum= a match{
+
+      case Some(v) => Datum(v)
+      case s:String => StringDatum(s)
+      case i:Int=>NumberDatum(i)
+      case f:Float =>NumberDatum(f)
+      case l:Long =>NumberDatum(l)
+      case b:Boolean => BooleanDatum(b)
+
+    }
+  }
 
 
-
+  object NoneDatum{
+    def apply()=new NoneDatum()
+  }
   class NoneDatum extends Datum {
 
-
+    override def termType:TokenType = p.Datum.DatumType.R_NULL
     def build(builder: p.Datum.Builder) ={
       builder
     }
@@ -200,13 +301,14 @@ object Ast {
 
   case class BooleanDatum(value: Boolean) extends Datum {
 
-
+    override def termType:TokenType = p.Datum.DatumType.R_BOOL
     def build(builder: p.Datum.Builder) ={
       builder.setType(p.Datum.DatumType.R_BOOL).setRBool(value)
     }
   }
 
   case class NumberDatum(value: Double) extends Datum {
+    override def termType:TokenType = p.Datum.DatumType.R_NUM
     def build(builder: p.Datum.Builder) = {
 
       builder.setType(p.Datum.DatumType.R_NUM).setRNum(value)
@@ -214,17 +316,19 @@ object Ast {
   }
 
   case class StringDatum(value: String) extends Datum {
+    def datumType:DatumTokenType =p.Datum.DatumType.R_STR
     def build(builder: p.Datum.Builder) = {
 
       builder.setType(p.Datum.DatumType.R_STR).setRStr(value)
     }
   }
+  //case class ArrayDatum(value:Seq[Any]) extends
 
 
-  case class MakeArray(array: Array[Any]) extends Term with Composable {
-    override lazy val args = buildArgs(for (a <- array) yield a)
+  case class MakeArray(array: Seq[Any]) extends Term with Composable {
+    override lazy val args = buildArgs(array: _*)
 
-    def termType = p.Term.TermType.MAKE_ARRAY
+    def termType:TokenType = p.Term.TermType.MAKE_ARRAY
 
 
     // do
@@ -235,28 +339,28 @@ object Ast {
   case class MakeObj(data: Map[String, Any]) extends Term {
     override lazy val optargs = buildOptArgs(data)
 
-    def termType = p.Term.TermType.MAKE_OBJ
+    def termType:TokenType = p.Term.TermType.MAKE_OBJ
   }
 
 
   case class Var(name: String) extends Term {
     override lazy val args = buildArgs(name)
 
-    def termType = p.Term.TermType.VAR
+    def termType:TokenType = p.Term.TermType.VAR
   }
 
   case class JavaScript(code: String) extends Term {
      override lazy val args=buildArgs(code)
-    def termType = p.Term.TermType.JAVASCRIPT
+    def termType:TokenType = p.Term.TermType.JAVASCRIPT
   }
 
   case class UserError(error: String) extends Term {
     override lazy val args=buildArgs(error)
-    def termType = p.Term.TermType.ERROR
+    def termType:TokenType = p.Term.TermType.ERROR
   }
 
   class ImplicitVar extends Term with Composable {
-    def termType = p.Term.TermType.IMPLICIT_VAR
+    def termType:TokenType = p.Term.TermType.IMPLICIT_VAR
   }
 
 
@@ -265,98 +369,98 @@ object Ast {
   }
 
   case class Eq(left: Term, right: Term) extends BiOperationTerm(left, right) {
-    def termType = p.Term.TermType.EQ
+    def termType:TokenType = p.Term.TermType.EQ
   }
 
   case class Ne(left: Term, right: Term) extends BiOperationTerm(left, right) {
-    def termType = p.Term.TermType.NE
+    def termType:TokenType = p.Term.TermType.NE
   }
 
   case class Lt(left: Term, right: Term) extends BiOperationTerm(left, right) {
-    def termType = p.Term.TermType.LT
+    def termType:TokenType = p.Term.TermType.LT
   }
 
   case class Le(left: Term, right: Term) extends BiOperationTerm(left, right) {
-    def termType = p.Term.TermType.LE
+    def termType:TokenType = p.Term.TermType.LE
   }
 
   case class Gt(left: Term, right: Term) extends BiOperationTerm(left, right) {
-    def termType = p.Term.TermType.GT
+    def termType:TokenType = p.Term.TermType.GT
   }
 
 
   case class Ge(left: Term, right: Term) extends BiOperationTerm(left, right) {
-    def termType = p.Term.TermType.GE
+    def termType:TokenType = p.Term.TermType.GE
   }
 
   case class Not(prev: Term) extends Term with Composable {
     override lazy val args = buildArgs(prev)
 
-    def termType = p.Term.TermType.NOT
+    def termType:TokenType = p.Term.TermType.NOT
   }
 
   case class Add(left: Term, right: Term) extends BiOperationTerm(left, right) {
-    def termType = p.Term.TermType.ADD
+    def termType:TokenType = p.Term.TermType.ADD
   }
 
   case class Sub(left: Term, right: Term) extends BiOperationTerm(left, right) {
-    def termType = p.Term.TermType.SUB
+    def termType:TokenType = p.Term.TermType.SUB
   }
 
   case class Mul(left: Term, right: Term) extends BiOperationTerm(left, right) {
-    def termType = p.Term.TermType.MUL
+    def termType:TokenType = p.Term.TermType.MUL
   }
 
   case class Div(left: Term, right: Term) extends BiOperationTerm(left, right) {
-    def termType = p.Term.TermType.DIV
+    def termType:TokenType = p.Term.TermType.DIV
   }
 
   case class Mod(left: Term, right: Term) extends BiOperationTerm(left, right) {
-    def termType = p.Term.TermType.MOD
+    def termType:TokenType = p.Term.TermType.MOD
   }
 
   case class Append(array: Array[Any], other: Term) extends Term {
     override lazy val args = buildArgs(array, other)
 
-    def termType = p.Term.TermType.APPEND
+    def termType:TokenType = p.Term.TermType.APPEND
   }
 
   case class Slice(target: Term, left: Int, right: Int) extends Term {
     override lazy val args = buildArgs(target, left, right)
 
-    def termType = p.Term.TermType.SLICE
+    def termType:TokenType = p.Term.TermType.SLICE
   }
 
   case class Skip(target: Term, amount: Int) extends Term with MethodTerm {
     override lazy val args = buildArgs(target, amount)
 
-    def termType = p.Term.TermType.SKIP
+    def termType:TokenType = p.Term.TermType.SKIP
   }
 
 
   case class Contains(target: Term, attributes: Seq[Any]) extends Term {
     override lazy val args = buildArgs(target, attributes)
 
-    def termType = p.Term.TermType.CONTAINS
+    def termType:TokenType = p.Term.TermType.CONTAINS
   }
 
 
   case class All(left: Term, right: Term) extends BiOperationTerm(left, right) {
-    def termType = p.Term.TermType.ALL
+    def termType:TokenType = p.Term.TermType.ALL
   }
 
   case class RAny(left: Term, right: Term) extends BiOperationTerm(left, right) {
-    def termType = p.Term.TermType.ANY
+    def termType:TokenType = p.Term.TermType.ANY
   }
 
 
   case class DB(name: String) extends Term {
     override lazy val args = buildArgs(name)
 
-    def termType = p.Term.TermType.DB
+    def termType:TokenType = p.Term.TermType.DB
 
-    def table_create(name: String, primaryKey: Option[String], dataCenter: Option[String], cacheSize: Option[Int]) {
-      TableCreate(name, primaryKey, dataCenter, cacheSize)
+    def table_create(name: String, primaryKey: Option[String]=None, dataCenter: Option[String]=None, cacheSize: Option[Int]=None)= {
+      TableCreate(this,name, primaryKey, dataCenter, cacheSize)
     }
 
     def ^^(name: String, primaryKey: Option[String], dataCenter: Option[String], cacheSize: Option[Int]) = this table_create(name, primaryKey, dataCenter, cacheSize)
@@ -366,7 +470,7 @@ object Ast {
 
     def ^-(name: String) = this table_drop (name)
 
-    def table(name: String, useOutDated: Boolean = false) = Table(name, useOutDated)
+    def table(name: String, useOutDated: Boolean = false) = Table(this,name, Some(useOutDated))
 
     def ^(name: String, useOutDated: Boolean = false) = this table(name, useOutDated)
 
@@ -374,46 +478,46 @@ object Ast {
   }
 
 
-  case class Insert(records: Seq[Map[String, Any]], upsert: Option[Boolean] = None) extends Term {
-    def name="INSERT"
-    override lazy val args = buildArgs(records)
+  case class Insert(table:Table,records: Seq[Map[String, Any]], upsert: Option[Boolean] = None) extends Term {
+
+    override lazy val args = buildArgs(table,records)
     override lazy val optargs = buildOptArgs(Map("upsert" -> upsert))
 
-    def termType = p.Term.TermType.INSERT
+    def termType:TokenType = p.Term.TermType.INSERT
   }
 
   case class Get(key: String) extends Term {
     override lazy val args = buildArgs(key)
 
-    def termType = p.Term.TermType.GET
+    def termType:TokenType = p.Term.TermType.GET
   }
 
-  case class TableCreate(name: String, primaryKey: Option[String] = None, dataCenter: Option[String] = None, cacheSize: Option[Int] = None) extends Term {
+  case class TableCreate(db:DB,name: String, primaryKey: Option[String] = None, dataCenter: Option[String] = None, cacheSize: Option[Int] = None) extends Term {
 
-    override lazy val args = buildArgs(name)
+    override lazy val args = buildArgs(db,name)
     override lazy val optargs = buildOptArgs(Map("name" -> name, "primary_key" -> primaryKey, "datacenter" -> dataCenter, "cache_size" -> cacheSize))
 
-    def termType = p.Term.TermType.TABLE_CREATE
+    def termType:TokenType = p.Term.TermType.TABLE_CREATE
   }
 
   case class TableDrop(name: String) extends Term with MethodTerm {
     override lazy val args = buildArgs(name)
 
-    def termType = p.Term.TermType.TABLE_DROP
+    def termType:TokenType = p.Term.TermType.TABLE_DROP
   }
 
   case class TableList(db: DB) extends Term with MethodTerm {
     override lazy val args=buildArgs(db)
-    def termType = p.Term.TermType.TABLE_LIST
+    def termType:TokenType = p.Term.TermType.TABLE_LIST
   }
 
-  case class Table(name: String, useOutDated: Option[Boolean] = None) extends Term with MethodTerm {
+  case class Table(db:DB,name: String, useOutDated: Option[Boolean] = None) extends Term with MethodTerm {
     override lazy val args = buildArgs(name)
     override lazy val optargs = buildOptArgs(Map("use_outdated" -> useOutDated))
 
-    def termType = p.Term.TermType.TABLE
+    def termType:TokenType = p.Term.TermType.TABLE
 
-    def insert(records: Seq[Map[String, Any]], upsert: Boolean = false) = Insert(records, Some(upsert))
+    def insert(records: Seq[Map[String, Any]], upsert: Boolean = false) = Insert(this,records, Some(upsert))
 
     def ++(records: Seq[Map[String, Any]], upsert: Boolean = false) = this insert(records, upsert)
 
--- src/main/scala/com/rethinkdb/utils/Printer.scala
+++ src/main/scala/com/rethinkdb/utils/Printer.scala
@@ -10,6 +10,7 @@ package com.rethinkdb.utils
 
 import com.rethinkdb.Ast._
 import ql2.{Ql2 => p}
+import com.rethinkdb.Tokens._
 
 
 trait Writer {
@@ -18,17 +19,26 @@ trait Writer {
   def write(value: String): Int
 }
 
+class ConsoleWriter extends Writer{
+  def write(value: String): Int = {
+    System.out.print(value)
+    value.replace("\n","").length
+  }
+}
 class StringBufferTermWriter extends Writer {
   private val builder = StringBuilder.newBuilder
 
   def write(value: String):Int= {
-    builder + value
-    value.length
+    builder ++= value
+    // get length without line ends
+    value.replace("\n","").length
+
 
   }
+  override def toString =builder.toString
 }
 
-class Tree(term: Term) {
+case class Tree(term: Term) {
 
   val line = "\n"
   val open="Term {"+line
@@ -55,6 +65,10 @@ class Tree(term: Term) {
     def +(add: Int) = copy(amount + add)
 
     def ++() = this + 1
+    def +++()=this + 2
+
+    def +>(extra:Int)=copy(amount + 2 + extra)
+
 
     def -(remove: Int) = copy(amount - remove)
 
@@ -64,7 +78,7 @@ class Tree(term: Term) {
 
   private val defaultIndent = Indent(0)
 
-  implicit def indent2String(indent: Indent) = " " * indent.amount
+  implicit def indent2String(indent: Indent) = "#" * indent.amount
   /*
 
 
@@ -88,30 +102,62 @@ class Tree(term: Term) {
 
   private def write(token: Token, indent: Indent = defaultIndent, applyIndent: Boolean = true)(implicit writer: Writer) {
     if(applyIndent) writer << indent
-    if(token.isInstanceOf[TermBlock]) writer<<"Term {"+line
+    val isTermBlock = token.isInstanceOf[TermBlock]
+    val offset = if(isTermBlock) writer<<"Term {"+line  else 0
     token match{
 
-      case a:Seq[_]=>{
+      case ap:AssocPairToken=>{
+        val extra= writer << "[\""+ap.key+"\"," +line
+        write(ap.token,indent+offset+extra)
+        writer<<"]"
+
+
+      }
+      case a:MakeArray=>{
+        write(a.termType,indent)
+        write(a.args,indent)
+      }
+      case o:OptArgs=>{
+        var offset = writer <<indent+"optargs = ["
+        for((p,index)<-o.optargs.zipWithIndex) write(p, indent +++, index != 0)
+
+
+
+        writer<<"];"+line
+      }
+      case a:Args=>{
         val offset = writer << "args = ["
-        for ((term, index) <- a.asInstanceOf[Seq[Term]].zipWithIndex) write(term, indent + offset, index != 0)
+        for ((term, index) <- a.args.zipWithIndex) write(term, indent + offset, index != 0)
+        writer<<indent+offset+"];"+line
       }
       case t:TokenType=>  {
-        writer << s"type = ${t.name};" + line
+        writer << s"type = ${t.name};"
       }
       case d:Datum=>{
 
-        write(d.termType,indent+2)
-        writer << indent + "r_datnum = Datnum { "
+        writer<< "type = R_DATUM;"+line
+
+        writer << (indent+++) + "r_datnum = Datnum { "
+        write(d.termType,indent+++)
         writer << (d match {
-          case b: BooleanDatum => s"r_bool = ${b.value};"
-          case n: NumberDatum => s"r_num = ${n.value};"
-          case s: StringDatum => "r_str = \""+s.value+"\";"
+          case b: BooleanDatum => s" r_bool = ${b.value};"
+          case n: NumberDatum => s" r_num = ${n.value};"
+          case s: StringDatum => " r_str = \""+s.value+"\";"
           case no:NoneDatum =>""
 
         })
         writer<<" };"+line
 
     }
+      case t:Term=>{
+        write(t.termType, indent +++)
+        writer <<   line
+        write(t.args, indent +++)
+        write(t.optargs,indent )
+
+
+        writer << indent+offset + "}"
+      }
     /*token match {
       case t: Term => {
 
@@ -144,8 +190,9 @@ class Tree(term: Term) {
         val offset = writer << "args = ["
         for ((term, index) <- args.args.zipWithIndex) write(term, indent + offset, index != 0)
       }  */
-      case _=>
+      case _=> writer <<"_blank_"
     }
+    if(isTermBlock) writer<<(indent+++)+"}"
   }
 }
 
