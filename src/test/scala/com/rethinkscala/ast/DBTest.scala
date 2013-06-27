1b0e4a9aade115d831443dda813d081c92f34a1f
Keyston <keyston@conceptual-ideas.com>
2013-06-27 15:35:04 -0500

    Adding missing tests

---
+++ src/test/scala/com/rethinkscala/ast/DBTest.scala
@@ -0,0 +1,70 @@
+package com.rethinkdscala.ast
+
+import org.scalatest.FunSuite
+
+import com.rethinkscala._
+import ql2._
+import com.rethinkdscala.BaseTest
+
+class DBTest extends FunSuite with BaseTest {
+
+  test("create db instance") {
+
+    val query = r.dbCreate("foo")
+    val ast = r.dbCreate("foo").ast
+
+    assert(ast, Term.TermType.DB_CREATE)
+    assert(ast.`args`.size == 1)
+    assert(ast.`args`(0), Term.TermType.DATUM)
+    assert(ast.`args`(0).`datum`, "foo")
+
+    assert(query, true)
+
+  }
+  test("list database") {
+
+    val query = r.dbs
+
+    assert[IS](query, {
+      x: IS => x.exists(a => a == "foo")
+    })
+  }
+  test("drop db instance") {
+
+    val query = r.dbDrop("foo")
+
+    assert[Boolean](query, true)
+  }
+
+  /*
+  test("create db instance"){
+   val db = DB("foo")
+
+    val term = db.toTerm
+
+    assert(term.getType == TermType.DB)
+    assert(term.getArgsCount == 1)
+    assert(term.getArgs(0).getType == TermType.DATUM)
+    assert(term.getArgs(0).getDatum.getRStr=="foo")
+
+  }
+  test("db with table"){
+
+
+    val db = DB("foo")
+    val term = db.table("bar",true).toTerm
+     System.out.println(term)
+    assert(term.getType ==TermType.TABLE)
+    assert(term.getArgsCount == 2)
+    assert(term.getArgs(0).getType == TermType.DB)
+    assert(term.getArgs(0).getArgs(0).getDatum.getRStr=="foo")
+
+    assert(term.getArgs(1).getType == TermType.DATUM)
+    assert(term.getArgs(1).getDatum.getRStr=="bar")
+
+    assert(term.getOptargsCount==1)
+    assert(term.getOptargs(0).getKey=="use_outdated")
+    assert(term.getOptargs(0).getVal.getDatum.getRBool == true)
+   // asser(term.getArgsCount)
+  } */
+}
