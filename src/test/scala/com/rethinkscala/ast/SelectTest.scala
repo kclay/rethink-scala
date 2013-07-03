b07b781bb68117fa6e4408fa8411e8a61c1aa4bd
keyston <keyston@conceptual-ideas.com>
2013-07-03 16:43:58 -0500

    Fix test suite

--- src/test/scala/com/rethinkscala/ast/SelectTest.scala
+++ src/test/scala/com/rethinkscala/ast/SelectTest.scala
@@ -20,9 +20,9 @@ class SelectTest extends FunSuite with BaseTest{
 
   test("select between"){
 
-    val records = for(i <-1 to  5) yield SelectFoo(i)
+    val records = for(i <-1 to  50) yield SelectFoo(i)
     table.insert(records).run
-    val results = table.between(2,4).order("id").as[SelectFoo]
+    val results = table.between(10,20).order("id").as[SelectFoo]
 
 
 
